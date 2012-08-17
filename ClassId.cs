ac79745f8eabbadb6aff3d59465785d91b08cf56
Omer Mor <omer.mor@gmail.com>
2012-08-18 00:06:46 +0300

    Start adding CoClass interfaces

---
+++ ClassId.cs
@@ -0,0 +1,9 @@
+namespace DirectShowLib
+{
+    internal static class ClassId
+    {
+        public const string StreamBufferSource = "C9F5FE02-F851-4eb5-99EE-AD602AF1E619";
+        public const string StreamBufferSink = "2DB47AE5-CF39-43c2-B4D6-0CD8D90946F4";
+        public const string SBE2Sink = "E2448508-95DA-4205-9A27-7EC81E723B1A";
+    }
+}
